79b54fce14004a15dccf7a406cb78d32258cbbd3
Dewan Ishtiaque Ahmed <30604461+dewan-ahmed@users.noreply.github.com>
2024-03-21 10:52:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
 def hello():
     return "Hello Gitness!"
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    print(hello())
